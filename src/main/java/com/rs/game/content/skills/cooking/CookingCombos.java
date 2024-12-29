8d061d4207c5273dc7b48046659052dda4cba109
MrAshieB <MrAshieB@users.noreply.github.com>
2024-12-29 21:17:51 +0000

    Cooking: Add pizza making (Plain, Meat, Anchovy & Pineapple).

--- src/main/java/com/rs/game/content/skills/cooking/CookingCombos.java
+++ src/main/java/com/rs/game/content/skills/cooking/CookingCombos.java
@@ -24,42 +24,54 @@ import com.rs.lib.game.Item;
 public class CookingCombos {
 
 	public enum CookingCombo {
-		PIE_SHELL(new Item(2315, 1), 1, new Item[] { new Item(2313, 1), new Item(1953, 1) }),
-		PART_MUD_PIE(new Item[] { new Item(7164, 1), new Item(1925, 1) }, 29, new Item[] { new Item(2315, 1), new Item(6032, 1) }),
-		PART_MUD_PIE2(new Item[] { new Item(7166, 1), new Item(1925, 1) }, 29, new Item[] { new Item(7164, 1), new Item(1929, 1) }),
-		RAW_MUD_PIE(new Item(7168, 1), 29, new Item[] { new Item(7166, 1), new Item(434, 1) }),
-		PART_GARDEN_PIE(new Item(7172, 1), 34, new Item[] { new Item(2315, 1), new Item(1982, 1) }),
-		PART_GARDEN_PIE2(new Item(7174, 1), 34, new Item[] { new Item(7172, 1), new Item(1957, 1) }),
-		RAW_GARDEN_PIE(new Item(7176, 1), 34, new Item[] { new Item(7174, 1), new Item(1965, 1) }),
-		PART_FISH_PIE(new Item(7182, 1), 47, new Item[] { new Item(2315, 1), new Item(333, 1) }),
-		PART_FISH_PIE2(new Item(7184, 1), 47, new Item[] { new Item(7182, 1), new Item(339, 1) }),
-		RAW_FISH_PIE(new Item(7186, 1), 47, new Item[] { new Item(7184, 1), new Item(1942, 1) }),
-		PART_ADMIRAL_PIE(new Item(7192, 1), 70, new Item[] { new Item(2315, 1), new Item(329, 1) }),
-		PART_ADMIRAL_PIE2(new Item(7194, 1), 70, new Item[] { new Item(7192, 1), new Item(361, 1) }),
-		RAW_ADMIRAL_PIE(new Item(7196, 1), 70, new Item[] { new Item(7194, 1), new Item(1942, 1) }),
-		PART_WILD_PIE(new Item(7202, 1), 85, new Item[] { new Item(2315, 1), new Item(2136, 1) }),
-		PART_WILD_PIE2(new Item(7204, 1), 85, new Item[] { new Item(7202, 1), new Item(2876, 1) }),
-		RAW_WILD_PIE(new Item(7206, 1), 85, new Item[] { new Item(7204, 1), new Item(3226, 1) }),
-		PART_SUMMER_PIE(new Item(7212, 1), 95, new Item[] { new Item(2315, 1), new Item(5504, 1) }),
-		PART_SUMMER_PIE2(new Item(7214, 1), 95, new Item[] { new Item(7212, 1), new Item(5982, 1) }),
-		RAW_SUMMER_PIE(new Item(7216, 1), 95, new Item[] { new Item(7214, 1), new Item(1955, 1) }),
-		UNCOOKED_APPLE_PIE(new Item(2317, 1), 30, new Item[] { new Item(2315, 1), new Item(1955, 1) }),
-		UNCOOKED_MEAT_PIE(new Item(2319, 1), 20, new Item[] { new Item(2315, 1), new Item(2140, 1) }),
-		UNCOOKED_BERRY_PIE(new Item(2321, 1), 10, new Item[] { new Item(2315, 1), new Item(1951, 1) });
+		PIE_SHELL(new Item(2315, 1), 1, 0, new Item[] { new Item(2313, 1), new Item(1953, 1) }),
+		PART_MUD_PIE(new Item[] { new Item(7164, 1), new Item(1925, 1) }, 29, 0, new Item[] { new Item(2315, 1), new Item(6032, 1) }),
+		PART_MUD_PIE2(new Item[] { new Item(7166, 1), new Item(1925, 1) }, 29, 0, new Item[] { new Item(7164, 1), new Item(1929, 1) }),
+		RAW_MUD_PIE(new Item(7168, 1), 29, 0, new Item[] { new Item(7166, 1), new Item(434, 1) }),
+		PART_GARDEN_PIE(new Item(7172, 1), 34, 0, new Item[] { new Item(2315, 1), new Item(1982, 1) }),
+		PART_GARDEN_PIE2(new Item(7174, 1), 34, 0, new Item[] { new Item(7172, 1), new Item(1957, 1) }),
+		RAW_GARDEN_PIE(new Item(7176, 1), 34, 0, new Item[] { new Item(7174, 1), new Item(1965, 1) }),
+		PART_FISH_PIE(new Item(7182, 1), 47, 0, new Item[] { new Item(2315, 1), new Item(333, 1) }),
+		PART_FISH_PIE2(new Item(7184, 1), 47, 0, new Item[] { new Item(7182, 1), new Item(339, 1) }),
+		RAW_FISH_PIE(new Item(7186, 1), 47, 0, new Item[] { new Item(7184, 1), new Item(1942, 1) }),
+		PART_ADMIRAL_PIE(new Item(7192, 1), 70, 0, new Item[] { new Item(2315, 1), new Item(329, 1) }),
+		PART_ADMIRAL_PIE2(new Item(7194, 1), 70, 0, new Item[] { new Item(7192, 1), new Item(361, 1) }),
+		RAW_ADMIRAL_PIE(new Item(7196, 1), 70, 0, new Item[] { new Item(7194, 1), new Item(1942, 1) }),
+		PART_WILD_PIE(new Item(7202, 1), 85, 0, new Item[] { new Item(2315, 1), new Item(2136, 1) }),
+		PART_WILD_PIE2(new Item(7204, 1), 85, 0, new Item[] { new Item(7202, 1), new Item(2876, 1) }),
+		RAW_WILD_PIE(new Item(7206, 1), 85, 0, new Item[] { new Item(7204, 1), new Item(3226, 1) }),
+		PART_SUMMER_PIE(new Item(7212, 1), 95, 0, new Item[] { new Item(2315, 1), new Item(5504, 1) }),
+		PART_SUMMER_PIE2(new Item(7214, 1), 95, 0, new Item[] { new Item(7212, 1), new Item(5982, 1) }),
+		RAW_SUMMER_PIE(new Item(7216, 1), 95, 0, new Item[] { new Item(7214, 1), new Item(1955, 1) }),
+		UNCOOKED_APPLE_PIE(new Item(2317, 1), 30, 0, new Item[] { new Item(2315, 1), new Item(1955, 1) }),
+		UNCOOKED_MEAT_PIE(new Item(2319, 1), 20, 0, new Item[] { new Item(2315, 1), new Item(2140, 1) }),
+		UNCOOKED_BERRY_PIE(new Item(2321, 1), 10, 0, new Item[] { new Item(2315, 1), new Item(1951, 1) }),
+
+		// Pizzas
+		INCOMPLETE_PIZZA(new Item(2285, 1), 35, 0, new Item[] { new Item(1982, 1), new Item(2283, 1) }),
+		UNCOOKED_PIZZA(new Item(2287, 1), 35, 0, new Item[] { new Item(2285, 1), new Item(1985, 1) }),
+		MEAT_PIZZA_MEAT(new Item(2293, 1), 45, 26, new Item[] { new Item(2289, 1), new Item(2142, 1) }),
+		MEAT_PIZZA_CHICKEN(new Item(2293, 1), 45, 26, new Item[] { new Item(2289, 1), new Item(2140, 1) }),
+		ANCHOVY_PIZZA(new Item(2297, 1), 55, 39, new Item[] { new Item(2289, 1), new Item(319, 1) }),
+		PINEAPPLE_PIZZA_RINGS(new Item(2301, 1), 65, 45, new Item[] { new Item(2289, 1), new Item(2118, 1) }),
+		PINEAPPLE_PIZZA_CHUNKS(new Item(2301, 1), 65, 45, new Item[] { new Item(2289, 1), new Item(2116, 1) });
 
 		private final Item[] product;
 		private final int req;
+		private final double xp;
 		private final Item[] materials;
 
-		private CookingCombo(Item product, int req, Item[] materials) {
+		private CookingCombo(Item product, int req, double xp, Item[] materials) {
 			this.product = new Item[] { product };
 			this.req = req;
+			this.xp = xp;
 			this.materials = materials;
 		}
 
-		private CookingCombo(Item[] product, int req, Item[] materials) {
+		private CookingCombo(Item[] product, int req, double xp, Item[] materials) {
 			this.product = product;
 			this.req = req;
+			this.xp = xp;
 			this.materials = materials;
 		}
 
@@ -80,7 +92,7 @@ public class CookingCombos {
 			}
 			if (!player.getInventory().containsItems(combo.materials))
 				return true;
-			player.startConversation(new CreateActionD(player, new Item[][] { combo.materials }, new Item[][] { combo.product }, new double[] { 0.0 }, new int[] { -1 }, new int[] { combo.req }, Constants.COOKING, 0));
+			player.startConversation(new CreateActionD(player, new Item[][] { combo.materials }, new Item[][] { combo.product }, new double[] { combo.xp}, new int[] { -1 }, new int[] { combo.req }, Constants.COOKING, 0));
 			return true;
 		}
 		return false;
